1f399f0d7e3d9a0400ad8837e593e19036d300a0
saki-0000 <saki_27_maruko@yahoo.co.jp>
2021-07-17 07:53:29 +0900

    戻し

--- tests/unit/Navbar.spec.ts
+++ tests/unit/Navbar.spec.ts
@@ -1,58 +1,62 @@
-import { mount } from '@vue/test-utils'
-import Navbar from '../../src/components/Navbar.vue'
-import Signup from '../../src/components/Signup.vue'
-import { Store } from '../../src/store'
+import { mount } from "@vue/test-utils";
+import Navbar from "../../src/components/Navbar.vue";
+import Signup from "../../src/components/Signup.vue";
+import { Store } from "../../src/store";
 
-jest.mock('vue-router', () => ({
+jest.mock("vue-router", () => ({
   useRouter: () => {
-    return {}
-  }
-}))
+    return {};
+  },
+}));
 
-describe('Navbar', () => {
-  it('shows a signup modal via teleport', async () => {
+describe("Navbar", () => {
+  it("shows a signup modal via teleport", async () => {
     const store = new Store({
       posts: {
         all: new Map(),
         ids: [],
-        loaded: false
-      },aaa
+        loaded: false,
+      },
       authors: {
         ids: [],
         all: new Map(),
         loaded: false,
-        currentUserId: undefined
-      }
-    })
+        currentUserId: undefined,
+      },
+    });
 
-    const el = document.createElement('div')
-    el.id = 'modal'
-    document.body.appendChild(el)
+    const el = document.createElement("div");
+    el.id = "modal";
+    document.body.appendChild(el);
 
     const wrapper = mount(Navbar, {
       attachTo: document.body,
       global: {
         components: {
           RouterLink: {
-            template: `<div></div>`
-          }
+            template: `<div></div>`,
+          },
         },
-        plugins: [
-          store,
-        ]
-      }
-    })
+        plugins: [store],
+      },
+    });
 
-    await wrapper.get('[data-test="sign-up"]').trigger('click')
-    const form = wrapper.getComponent(Signup)
+    await wrapper
+      .get('[data-test="sign-up"]')
+      .trigger("click");
+    const form = wrapper.getComponent(Signup);
 
-    expect(document.body.outerHTML).toContain('The value must be between 10 and 40')
+    expect(document.body.outerHTML).toContain(
+      "The value must be between 10 and 40"
+    );
 
-    await form.get('#Username').setValue('Username')
-    await form.get('#Password').setValue('12345678910')
+    await form.get("#Username").setValue("Username");
+    await form.get("#Password").setValue("12345678910");
 
-    expect(document.body.outerHTML).not.toContain('The value must be between 10 and 40')
+    expect(document.body.outerHTML).not.toContain(
+      "The value must be between 10 and 40"
+    );
 
-    await form.trigger('submit.prevent')
-  })
-})
+    await form.trigger("submit.prevent");
+  });
+});
